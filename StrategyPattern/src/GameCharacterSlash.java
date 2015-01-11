d88f6ac128bc9e36154a8dd36f5b961fe09f8d8d
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-01-10 16:42:41 -0800

    Remove
    Remove

--- StrategyPattern/src/GameCharacterSlash.java
+++
@@ -1,9 +0,0 @@
-
-public class GameCharacterSlash extends GameCharacter
-{
-	public GameCharacterSlash()
-	{
-		super.setGuitarType(new Gibson());
-		super.setSoloType(new SlashSolo());
-	}
-}
--- StrategyPattern/src/GuitarBehavior.java
+++
@@ -1,5 +0,0 @@
-
-public interface GuitarBehavior
-{
-	public void playGuitar();
-}
