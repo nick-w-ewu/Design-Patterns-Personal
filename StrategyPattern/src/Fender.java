70979b94ceb3eed2c616f34943ce2e4051bc937a
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-01-10 16:43:38 -0800

    Fix

---
+++ StrategyPattern/src/Fender.java
@@ -0,0 +1,11 @@
+
+public class Fender implements GuitarBehavior
+{
+
+	@Override
+	public void playGuitar()
+	{
+		System.out.println("I'm playing the Fender Telecaster");
+	}
+
+}
---
+++ StrategyPattern/src/YoungSolo.java
@@ -0,0 +1,11 @@
+
+public class YoungSolo implements SoloBehavior
+{
+
+	@Override
+	public void performSolo()
+	{
+		System.out.println("(Smashes the guitar) kiai!!!!");
+	}
+
+}
